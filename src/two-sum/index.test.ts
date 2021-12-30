306293fae8b74862c4923ad0a3ff6109f3eb81c0
jayakrishnan cn <jayakrishnancn@gmail.com>
2021-12-30 15:41:46 +0530

    two sum

--- src/two-sum/index.test.ts
+++ src/two-sum/index.test.ts
@@ -1,5 +1,10 @@
-describe("two-sum", () => {
-  it("two-sum", () => {
-    expect(true).toEqual(true);
+import Default from './index';
+
+describe('two-sum', () => {
+  it('case 1', () => {
+    expect(Default([2, 7, 11, 15], 9)).toEqual([0, 1]);
+  });
+  it('case 2', () => {
+    expect(Default([3, 2, 4], 6)).toEqual([1, 2]);
   });
 });
--- src/two-sum/index.ts
+++ src/two-sum/index.ts
@@ -3,3 +3,18 @@
  * Given an array of integers nums and an integer target,
  * return indices of the two numbers such that they add up to target.
  */
+function twoSum(nums: number[], target: number): number[] {
+  let trackedNums: { [a: number]: number } = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const opp = target - nums[i],
+      num = nums[i];
+    if (trackedNums[num] >= 0) {
+      return [trackedNums[num], i];
+    }
+    trackedNums[opp] = i;
+  }
+  return [];
+}
+
+export default twoSum;
