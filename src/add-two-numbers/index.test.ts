e930a4e6af16e9138db97a1db3911f5b767916d3
jayakrishnan cn <jayakrishnancn@gmail.com>
2022-01-04 22:58:01 +0530

    addTwoNumbers

--- src/add-two-numbers/index.test.ts
+++ src/add-two-numbers/index.test.ts
@@ -1,7 +1,32 @@
-import Default from ".";
+import Default, { ListNode } from ".";
+
+const getList = arr => {
+  let head, prev;
+  arr.forEach(val => {
+    let node = new ListNode(val);
+    if (!head) {
+      head = node;
+      prev = node;
+    } else if (prev) {
+      prev.next = node;
+      prev = prev.next;
+    }
+  });
+
+  return head;
+};
 
 describe("two-sum", () => {
   it("case 1", () => {
-    expect(Default("any params")).toEqual(0);
+    let a1 = getList([2, 4, 3]);
+    let a2 = getList([5, 6, 4]);
+    let a3 = getList([7, 0, 8]);
+    expect(Default(a1, a2)).toEqual(a3);
+  });
+  it("case 2", () => {
+    let a1 = getList([1]);
+    let a2 = getList([9]);
+    let a3 = getList([0, 1]);
+    expect(Default(a1, a2)).toEqual(a3);
   });
 });
--- src/add-two-numbers/index.ts
+++ src/add-two-numbers/index.ts
@@ -1,5 +1,46 @@
-function fnName(args: any): any {
-  return 0;
+// Definition for singly-linked list.
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
 }
 
-export default fnName;
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  let head: ListNode | null = null;
+  let carry = 0,
+    prev: ListNode | null = null;
+  while (l1 || l2) {
+    let val = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+    l1 = l1?.next ?? null;
+    l2 = l2?.next ?? null;
+    carry = val > 9 ? 1 : 0;
+    let node = new ListNode(val % 10);
+    if (!head) {
+      head = node;
+      prev = node;
+    } else if (prev) {
+      prev.next = node;
+      prev = prev.next;
+    }
+  }
+  if (carry) {
+    let node = new ListNode(1);
+    if (!head) {
+      head = node;
+      prev = node;
+    } else if (prev) {
+      prev.next = node;
+      prev = prev.next;
+    }
+  }
+
+  return head;
+}
+
+export default addTwoNumbers;
