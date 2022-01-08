25da62a5fe3298d4913516a520172be5b1dd0763
jayakrishnan cn <jayakrishnancn@gmail.com>
2022-01-08 22:10:15 +0530

    temp

--- src/cherry-pickup-ii/index.ts
+++ src/cherry-pickup-ii/index.ts
@@ -1,58 +1,47 @@
-// mxn
-let cache: any = {};
-
 function cherryPickup(grid: number[][]): number {
-  function travel(row, c1, c2) {
-    let r = grid[row];
-    if (
-      row < 0 ||
-      c1 < 0 ||
-      c2 < 0 ||
-      row > grid.length - 1 ||
-      c1 > grid[0].length - 1 ||
-      c2 > grid[0].length - 1
-    ) {
+  const n = grid.length,
+    m = grid[0].length;
+  // 4D DP array
+  let memo = {};
+
+  function solve(r1, c1, c2) {
+    // if invalid state return -ve
+    if (r1 >= n || c1 >= m || c2 >= m || c1 < 0 || c2 < 0) {
       return 0;
     }
+    // if stored in cache return from dp state
+    if (memo[`${r1}${c1}${c2}`])
+      return memo[`${r1}${c1}${c2}`];
 
-    if (cache?.[row]?.[c1]?.[c2]) {
-      return cache[row][c1][c2];
+    if (r1 == n - 1) {
+      if (c1 == c2) return grid[r1][c1];
+      // if both arrive at the same cell add only once
+      else return grid[r1][c1] + grid[r1][c2]; // if both are at different cells add both
     }
-
-    if (c1 === c2) {
-      if (r[c2 + 1] > r[c2 - 1]) {
-        c2 = c2 + 1;
-      } else {
-        c1 = c2 - 1;
-      }
+    let cherries = 0;
+    if (c1 == c2) cherries += grid[r1][c1];
+    else {
+      cherries += grid[r1][c1] + grid[r1][c2];
+      // recursive calls for all possible 9 choices
+      // each robot has 3 choices => hence total choices = 3*3 =9
+      let d1 = solve(r1 + 1, c1 - 1, c2 - 1);
+      let d2 = solve(r1 + 1, c1 - 1, c2);
+      let d3 = solve(r1 + 1, c1 - 1, c2 + 1);
+      let d4 = solve(r1 + 1, c1, c2 - 1);
+      let d5 = solve(r1 + 1, c1, c2);
+      let d6 = solve(r1 + 1, c1, c2 + 1);
+      let d7 = solve(r1 + 1, c1 + 1, c2 - 1);
+      let d8 = solve(r1 + 1, c1 + 1, c2);
+      let d9 = solve(r1 + 1, c1 + 1, c2 + 1);
+      // add optimal choice to ans
+      let mx = Math.max(d1, d2, d3, d4, d5, d6, d7, d8, d9);
+      cherries += mx;
     }
-    let result = r[c1] ?? 0;
-    if (c1 !== c2) {
-      result += r[c2] ?? 0;
-    }
-
-    if (row < grid.length) {
-      let max = 0;
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          max = Math.max(
-            max,
-            travel(row + 1, c1 - 1 + i, c2 - 1 + j)
-          );
-        }
-      }
-      result += max;
-    }
-    if (!cache[row]) {
-      cache[row] = {};
-    }
-    cache[row][c1] = {
-      [c2]: result,
-    };
-
-    return result;
+    // memoize and return
+    return (memo[`${r1}${c1}${c2}`] = cherries);
   }
-
-  return travel(0, 0, grid[0].length - 1);
+  // let64_t to avoid overflow
+  return solve(0, 0, 0);
 }
+
 export default cherryPickup;
