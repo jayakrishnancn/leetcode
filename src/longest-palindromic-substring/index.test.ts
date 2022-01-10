806b68542c12cc10b345142c69f684e5a41f0b68
jayakrishnan cn <jayakrishnancn@gmail.com>
2022-01-10 16:16:02 +0530

    Longest Palindromic Substring

---
+++ src/longest-palindromic-substring/index.test.ts
@@ -0,0 +1,27 @@
+import Default from ".";
+
+describe("two-sum", () => {
+  it("case 1", () => {
+    expect(
+      Default(
+        "zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir"
+      )
+    ).toEqual("gykrkyg");
+  });
+
+  it("case 2", () => {
+    expect(Default("a")).toEqual("a");
+  });
+  it("case 3", () => {
+    expect(Default("aba")).toEqual("aba");
+  });
+  it("case 4", () => {
+    expect(Default("babad")).toEqual("bab");
+  });
+  it("case 5", () => {
+    expect(Default("cbbd")).toEqual("bb");
+  });
+  it("case 6", () => {
+    expect(Default("ccc")).toEqual("ccc");
+  });
+});
