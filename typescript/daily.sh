27ad929f4d4eaf591dfc30ec67a5bebd0e4a67c4
jayakrishnancn <jayakrishnancn@gmail.com>
2024-06-25 23:26:20 +0530

    daily.sh

--- typescript/daily.sh
+++ typescript/daily.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-	echo "No arguments supplied"
+if [ $# -ne 2 ]; then
+	echo "2 arguments are required <dir_name> <question_number>"
 	exit
 fi
 
@@ -15,5 +15,14 @@ done
 
 mkdir -p $Directory
 cp -a ./src/templates/daily/. "$Directory"
+
+full_path="$(realpath "$Directory")"
+
+if [ -n "$2" ]; then
+	ln -s "$full_path" "$(pwd)/src/problems/$2"
+	echo "Symbolic link created from $full_path to src/$2"
+else
+	echo "Second parameter not provided. Usage: $0 <dir_name> <2nd parameter>"
+fi
 
 pnpm jest $Directory --watch --verbose false
