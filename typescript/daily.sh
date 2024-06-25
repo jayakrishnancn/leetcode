f736b4a1439ea682410fdd9b935bda39e8792a47
jayakrishnancn <jayakrishnancn@gmail.com>
2024-06-25 23:36:47 +0530

    daily

--- typescript/daily.sh
+++ typescript/daily.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-	echo "2 arguments are required <dir_name> <question_number>"
+if [ $# -eq 0 ]; then
+	echo "No arguments passed, ./new.sh <question_number>"
 	exit
 fi
 
-Directory="./src/daily/$1"
+Directory="./src/problems/$1"
 
 while [ -d $Directory ]; do
 	echo "$Directory is a directory"
 	read -p "another name:" name
-	Directory="./src/daily/$name"
+	Directory="./src/problems/$name"
 done
 
 mkdir -p $Directory
@@ -19,10 +19,8 @@ cp -a ./src/templates/daily/. "$Directory"
 full_path="$(realpath "$Directory")"
 
 if [ -n "$2" ]; then
-	ln -s "$full_path" "$(pwd)/src/problems/$2"
+	ln -s "$full_path" "$(pwd)/src/daily/$2"
 	echo "Symbolic link created from $full_path to src/$2"
-else
-	echo "Second parameter not provided. Usage: $0 <dir_name> <2nd parameter>"
 fi
 
-pnpm jest $Directory --watch --verbose false
+# pnpm jest $Directory --watch --verbose false
