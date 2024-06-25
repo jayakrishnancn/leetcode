25cf964a00c65db9a510786c732683b6deb6fc4e
jayakrishnancn <jayakrishnancn@gmail.com>
2024-06-25 22:41:14 +0530

    June 25

--- new.sh
+++ new.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-    exit;
+if [ $# -eq 0 ]; then
+	echo "No arguments supplied"
+	exit
 fi
 
 Directory="./src/$1"
 
-while [ -d $Directory ] ; do
-    echo "$Directory is a directory";
-    read -p "another name:" name
-    Directory="./src/$name"
+while [ -d $Directory ]; do
+	echo "$Directory is a directory"
+	read -p "another name:" name
+	Directory="./src/$name"
 done
 
-cp ./templates/ts/. $Directory -R
-yarn jest $Directory --watch --verbose false
+mkdir -p $Directory
+cp -a ./templates/ts/. "$Directory"
+pnpm jest $Directory --watch --verbose false
